0bcd7aaa74b43850218fdd0bbbbb74726abdca42
Inderjot Singh <inderjotsingh141@gmail.com>
2024-04-13 10:22:32 +0530

    features animation

--- frontend/src/components/Feature.tsx
+++ frontend/src/components/Feature.tsx
@@ -19,37 +19,21 @@ const data = {
         {
             "title": "Connect with anyone on the internet",
             "description": "Connect with anyone, anywhere in the world with just a few clicks.",
-            "image": "/features/one.jpg"
+            "image": "/features/two.jpg"
         },
         {
             "title": "Engagement with gesture recognition",
             "description": "Engage with your audience using gesture recognition to control your presence on the call.",
-            "image": "/features/one.jpg"
+            "image": "/features/four.jpg"
         },
         {
             "title": "Chat features give you more control over your presence",
             "description": "Chat features allow you to have more control over your presence during the call.",
-            "image": "/features/one.jpg"
+            "image": "/features/three.png"
         }
     ]
 }
 
-
-const variants = {
-    "enter": {
-        opacity: [0, 1],
-        y: [30, 0],
-        z: [2, 0],
-        transition: { duration: 0.5 }
-    },
-    "exit": {
-        opacity: [1, 0],
-        y: [0, 30],
-        z: [0, 2],
-        transition: { duration: 0.5 }
-    }
-
-} satisfies Variants
 
 export const Features = (props: Props) => {
 
@@ -84,29 +68,47 @@ export const Features = (props: Props) => {
     })
 
     return (
-        <motion.div ref={ref} className={`${height}`} >
+        <motion.div ref={ref} className={`h-[400vh]`} >
 
             <div className='sticky flex top-[130px] w-4/5 mx-auto h-[450px]  '>
-                <div className='flex flex-col h-full'>
-                    <motion.div className='relative w-1 h-full bg-[#25AEB8] rounded-b-full' style={{ scaleY: scrollYProgress, transformOrigin: "top" }} >
+                <div className='flex flex-col items-center h-full'>
+                    <span className='font-mono' >01</span>
+                    <motion.div className='relative w-1 h-full  bg-gradient-to-b from-teal-300 to-teal-600   rounded-b-full' style={{ scaleY: scrollYProgress, transformOrigin: "top" }} >
                     </motion.div>
+                    <span className='font-mono' >04</span>
                 </div>
 
                 <div className='flex flex-col my-10 mx-8 text-muted-foreground justify-between'>
                     {
                         keys.map((key, index) => (
-                            <div className={'flex flex-col gap-1'} key={key.title}>
-                                <div className={cn('text-2xl', index === activeNum && "text-orange-500")} >
+                            <div className={cn(index === activeNum && '', 'flex relative flex-col gap-1 p-3 rounded-md')} key={`${key.title}-${index}`}  >
+                                <motion.div className={cn('text-xl', index === activeNum && "text-black font-medium")} >
                                     {key.title}
-                                </div>
+                                </motion.div>
+
+                                {
+                                    index === activeNum
+                                    &&
+                                    <motion.div
+                                        transition={{ duration: 1.3 }}
+                                        layoutId='hover-blob' className='absolute inset-0 -z-10 bg-[#FEF4EB]' ></motion.div>
+                                }
 
 
                                 {
                                     (index === activeNum) &&
-                                    <motion.div
-                                        className='' >
-                                        {key.description}
-                                    </motion.div>
+                                    <AnimatePresence>
+                                        <motion.div
+                                            initial={{ opacity: 0 }}
+                                            animate={{ opacity: 1 }}
+                                            exit={{ opacity: 0 }}
+
+                                            transition={{ duration: 1.5 }}
+                                            key={`${key.title}-${index}`}
+                                            className='text-sm' >
+                                            {key.description}
+                                        </motion.div>
+                                    </AnimatePresence>
                                 }
 
 
@@ -115,13 +117,14 @@ export const Features = (props: Props) => {
                     }
                 </div>
 
-                <div className='flex flex-col justify-center ml-auto pr-5 md:pr-10 lg:pr-28' >
-                    <AnimatePresence>
+                <div className='flex flex-col justify-center ml-auto pr-5 md:pr-10 lg:pr-16' >
+                    <AnimatePresence mode='wait'>
                         <motion.div
                             key={activeNum}
-                            variants={variants}
-                            animate={"enter"}
-                            exit={"exit"}
+                            initial={{ y: 30, opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            exit={{ y: -30, opacity: 0 }}
+                            transition={{ duration: 0.4 }}
                             className='relative  size-[250px] md:w-[250px] md:h-[200px]   lg:w-[450px] lg:h-[300px] overflow-hidden rounded-md '>
                             <Image
                                 className='absolute inset-0 object-cover'
